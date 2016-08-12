9e07a9c2972000cc395fd0bb26e15baa34f28415
Hamza Khan <hamzakhan@Fatimas-MacBook-Pro-2.local>
2016-08-12 14:44:04 -0400

    final changes

---
+++ exercise6.rb
@@ -0,0 +1,19 @@
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+grocery_list.each do |item|
+  puts "* #{item}"
+end
+
+
+
+
+
+
+
+
+#instances have instance variables and instance methods
+# classes have class variables and class methods
+# instance is an instance of a class
+# cant have classes of the same name, but different names
+# class variable = total number of books
+# instance variable = total number of times checked out
